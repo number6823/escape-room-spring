ea7ce82733c6a436f87d1391062559389aed2204
csmbl <ohdehet@naver.com>
2026-09-15 21:09:32 +0900

    초기화

---
+++ src/main/java/com/escape/room/escaperoombackend/EscapeRoomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.escape.room.escaperoombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeRoomBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscapeRoomBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/escape/room/escaperoombackend/EscapeRoomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escape.room.escaperoombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeRoomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
